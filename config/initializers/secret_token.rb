5c2af97f22f00d7bdbf3fc334b46c1958419869a
Luke Booth <Luke.Booth@cph.org>
2012-09-19 14:35:10 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InnovationCenterManuals::Application.config.secret_token = '1dc019b498186b2e5c5dcd54ed0bc4e28cbb9b8496977d3292e7a64a87eb5d0d011ef4d3916a3756ccf58a7a295f846c36bc60bbe1dcd9fb95d529ccb6afd2c4'

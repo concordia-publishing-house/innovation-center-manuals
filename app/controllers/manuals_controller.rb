1580b5fe725365cc57671a87965958ba0065b86d
Kendall Park <kendall.park@cph.org>
2013-04-23 13:48:11 -0500

    deleted all the old files

--- app/controllers/manuals_controller.rb
+++
@@ -1,4 +0,0 @@
-class ManualsController < ApplicationController
-  def index
-  end
-end
--- config/deploy.rb
+++
@@ -1,3 +0,0 @@
-set :application, "innovation-center-manuals"
-load File.expand_path("~/epic.cap")
-set :branch, "master"
